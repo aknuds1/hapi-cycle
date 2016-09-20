237aae46881978861d348bd80afc50ad93ec7510
Arve Knudsen <arve.knudsen@gmail.com>
2016-09-20 12:22:34 +0100

    Simplify

--- app/server.js
+++ app/server.js
@@ -4,9 +4,7 @@ import Hapi from 'hapi'
 import R from 'ramda'
 import path from 'path'
 import Boom from 'boom'
-import pug from 'pug'
 import Url from 'url'
-import Promise from 'bluebird'
 import Logger from '@arve.knudsen/js-logger'
 let logger = Logger.get('server')
 Logger.useDefaults({
@@ -18,13 +16,6 @@ Logger.useDefaults({
 import rendering from './server/rendering'
 
 let server = new Hapi.Server({
-  connections: {
-    routes: {
-      files: {
-        relativeTo: path.join(__dirname, '../public'),
-      },
-    },
-  },
 })
 let port = parseInt(process.env.PORT || '8000')
 server.connection({
@@ -32,90 +23,21 @@ server.connection({
   port,
 })
 
-if (process.env.APP_URI == null) {
-  process.env.APP_URI = `http://localhost:${port}`
-}
-
-let hapiPlugins = R.map((x) => {return require(x)}, ['inert', 'vision',])
-server.register(hapiPlugins, (err) => {
+server.register([require('inert'),], (err) => {
   if (err != null) {
     throw err
   }
 
   let routeServerMethod = (options) => {
-    if (typeof options.handler === 'function') {
-      let originalHandler = options.handler
-      options.handler = (request, reply) => {
-        Promise.method(originalHandler)(request, reply)
-          .catch((error) => {
-            logger.error(`An unhandled error occurred:`, error.stack)
-            reply(Boom.badImplementation())
-          })
-      }
-    }
     server.route(R.merge({
       method: `GET`,
     }, options))
   }
 
-  server.ext('onRequest', (request, reply) => {
-    if (request.headers['x-forwarded-proto'] === 'http') {
-      console.log(`Redirecting to HTTPS, using $APP_URI: ${process.env.APP_URI}`)
-      return reply.redirect(Url.format({
-        protocol: 'https',
-        hostname: Url.parse(process.env.APP_URI).hostname,
-        pathname: request.url.path,
-        port: 443,
-      }))
-    } else {
-      console.log(`Not redirecting to HTTPS`)
-      reply.continue()
-    }
-  })
-
-  server.views({
-    engines: {pug,},
-    path: __dirname + '/templates',
-    compileOptions: {
-      pretty: true,
-    },
-  })
-
   routeServerMethod({
     path: '/{path*}',
     handler: rendering.renderIndex,
   })
-  server.route({
-    method: ['GET',],
-    path: '/bundle.js',
-    handler: {
-      file: path.join(__dirname, '../dist/bundle.js'),
-    },
-  })
-  server.route({
-    method: 'GET',
-    path: '/assets/{param*}',
-    handler: {
-      directory: {
-        path: '.',
-        redirectToSlash: true,
-        listing: true,
-      },
-    },
-  })
-  server.route({
-    method: 'GET',
-    path: '/robots.txt',
-    handler: (request, reply) => {
-      if ((process.env.NODE_ENV || '').toLowerCase() !== 'production') {
-        reply(`User-agent: *\nDisallow: /`).header('Content-Type', 'text/plain')
-      } else {
-        reply()
-      }
-    },
-  })
-
-  // api.register(server)
 
   server.start((err) => {
     if (err != null) {
--- app/server/rendering.js
+++ app/server/rendering.js
@@ -1,38 +1,9 @@
 import Cycle from '@cycle/xstream-run'
 import xs from 'xstream'
 import {html, section, h1, p, head, title, body, div, script, makeHTMLDriver,} from '@cycle/dom'
-import R from 'ramda'
-import Boom from 'boom'
 import serialize from 'serialize-javascript'
 import Logger from '@arve.knudsen/js-logger'
 let logger = Logger.get('server.rendering')
-
-let getInitialRouterState = (request) => {
-  let currentPath = request.path
-  logger.debug(`Computing initial router state, path is '${currentPath}'`)
-  let navItems = R.map((navItem) => {
-    let path = !navItem.isExternal ? normalizePath(navItem.path) : navItem.path
-    let isSelected = path === currentPath
-    logger.debug(`Nav item '${navItem.text}' is selected: ${isSelected}, ${path}`)
-    return R.merge(navItem, {
-      path,
-      isSelected,
-    })
-  }, [
-    {path: '/', text: 'Calendar',},
-    {path: '/about', text: 'About',},
-  ])
-  if (!R.any((navItem) => {return navItem.isSelected}, navItems)) {
-    logger.debug(`Defaulting to root nav item being selected`)
-    let navItem = R.find((navItem) => {return navItem.path === '/'}, navItems)
-    navItem.isSelected = true
-  }
-  return immutable.fromJS({
-    currentPath,
-    isLoading: false,
-    navItems,
-  })
-}
 
 let wrapVTreeWithHtmlBoilerplate = ([vtree, context,]) => {
   return (
@@ -49,24 +20,6 @@ let wrapVTreeWithHtmlBoilerplate = ([vtree, context,]) => {
   );
 }
 
-// let clientBundle = (() => {
-//   let bundle$ = xs.createWithMemory()
-//   let bundleString = ''
-//   let bundleStream = browserify()
-//     .transform('babelify')
-//     .transform({global: true,}, 'uglifyify')
-//     .add('./client.js')
-//     .bundle()
-//   bundleStream.on('data', function (data) {
-//     bundleString += data;
-//   })
-//   bundleStream.on('end', function () {
-//     bundle$.shamefullySendNext(bundleString);
-//     console.log('Client bundle successfully compiled.');
-//   });
-//   return bundle$;
-// })()
-
 let main = (sources) => {
   let vtree = (
     section('.home', [
@@ -82,7 +35,6 @@ let main = (sources) => {
 let renderIndex = (request, reply) => {
   let context = xs.of({})
   Cycle.run((sources) => {
-    logger.debug(`Running`, sources)
     let vtree = main(sources).DOM
     let wrappedVTree = xs.combine(vtree, context)
       .map(wrapVTreeWithHtmlBoilerplate)
@@ -92,9 +44,7 @@ let renderIndex = (request, reply) => {
     };
   }, {
     DOM: makeHTMLDriver((html) => {
-      logger.debug(`Rendering`)
       let wrappedHtml = `<!doctype html>${html}`
-      logger.debug(`Rendered HTML`, wrappedHtml)
     }),
     context: () => {return context},
     PreventDefault: () => {},
